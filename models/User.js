2372e507a4b824e1527ca5c37668330ff716b632
Yolanda Jacobs <y.jacobs@hotmail.com>
2022-08-21 16:10:05 -0400

    user routes and controllers

---
+++ models/User.js
@@ -0,0 +1,20 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+    }
+});
+
+const User = model('User', UserSchema);
+
+module.exports = User;
